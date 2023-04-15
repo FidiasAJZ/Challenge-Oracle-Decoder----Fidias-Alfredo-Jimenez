80c9ade8f073281c8de4193271374bbf6df4fa35
Fidias <fidiasxalfredoz@gmail.com>
2023-04-15 15:29:38 -0600

    validationsv1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 //Documentación de donde aprendí a utilizar sweetAlert2
 //https://sweetalert2.github.io
 
+//Validar los campos del textarea
+//El texto se borra cuando se tiene el foco.
+//document.getElementById("inputTexto").value = "";
+
+//Dejo la documentacion donde encontre la tabla ASCII para hacer la validacion
+//https://elcodigoascii.com.ar
+
+function validarTexto(){
+    if(document.getElementById("inputTexto").value == "Ingrese el texto aquí"){
+        document.getElementById("inputTexto").value = "";
+    }
+}
+
+//Cuando la caja de texto esta vacia y pierde el foco, agrega un texto.
+function agregarTexto() {
+    var miTextArea = document.getElementById("inputTexto");
+    if (miTextArea.value == "") {
+      miTextArea.value = "Ingrese aquí el texto";
+    }
+  }
+  
+
 function copiar(){
     var contenido = document.querySelector("#areaTexto");
     contenido.select(); //Seleccionamos el texto en el textarea.
@@ -16,55 +38,61 @@ function copiar(){
 }
 
 function encriptar(){
-    let texto = document.getElementById("inputTexto").value.toLowerCase();
-    //i en el codigo es para que afecte tanto a mayusculas como minusculas en el campo de texto.
-    //g, en el codigo es para que afecte toda la linea u oracion .
-    //m, es para que afecte a multiples lineas o parrafos.
-    var cifrado = texto.replace(/e/igm,"enter");
-    var cifrado = cifrado.replace(/o/igm,"ober");
-    var cifrado = cifrado.replace(/i/igm,"imes");
-    var cifrado = cifrado.replace(/a/igm,"ai");
-    var cifrado = cifrado.replace(/u/igm,"ufat");
-
-    document.getElementById("muneco").style.display = "none";
-    document.getElementById("textoEncontrado").style.display = "none";
-    document.getElementById("areaTexto").innerHTML = cifrado;
-    document.getElementById("copiar").style.display = "show";//Mostramos el boton de copiar.
-    document.getElementById("copiar").style.display = "inherit"; //Inherit toma el valor calculado de la propiedad de su elemento padre.
-
-    //Codigo para utilizar el sweetalert2
-    let timerInterval
-    Swal.fire({
-    title: 'Encriptando!',
-    html: 'Cargando <b></b>',
-    timer: 500,
-    timerProgressBar: true,
-    imageUrl: "../img/catpro.gif",
-    imageWidth: 400,
-    imageHeight: 400,
-    imageAlt: 'Custom image',
-/*     backdrop: `
-            rgba(0,0,123,0.4)
-            url("../img/code.gif")
-            left top
-            no-repeat
-        `, */
-    didOpen: () => {
-        Swal.showLoading()
-        const b = Swal.getHtmlContainer().querySelector('b')
-        timerInterval = setInterval(() => {
-        b.textContent = Swal.getTimerLeft()
-        }, 100)
-    },
-    willClose: () => {
-        clearInterval(timerInterval)
-    }
-    }).then((result) => {
-    /* Read more about handling dismissals below */
-    if (result.dismiss === Swal.DismissReason.timer) {
-        console.log('I was closed by the timer')
-    }
-    })
+    var miTextArea = document.getElementById("inputTexto");
+    var texto = miTextArea.value; // Obtener el texto del textarea
+    var expReg = /^[a-z0-9\s]+$/; // Expresión regular para validar caracteres especiales
+  
+    if (!expReg.test(texto)) {
+      // Si el texto contiene caracteres especiales, mostrar una alerta
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'No se permiten letras mayúsculas, caracteres especiales, tildes ni números!'
+      })
+    }else{
+        let texto = document.getElementById("inputTexto").value.toLowerCase();
+        //i en el codigo es para que afecte tanto a mayusculas como minusculas en el campo de texto.
+        //g, en el codigo es para que afecte toda la linea u oracion .
+        //m, es para que afecte a multiples lineas o parrafos.
+        var cifrado = texto.replace(/e/igm,"enter");
+        var cifrado = cifrado.replace(/o/igm,"ober");
+        var cifrado = cifrado.replace(/i/igm,"imes");
+        var cifrado = cifrado.replace(/a/igm,"ai");
+        var cifrado = cifrado.replace(/u/igm,"ufat");
+    
+        document.getElementById("muneco").style.display = "none";
+        document.getElementById("textoEncontrado").style.display = "none";
+        document.getElementById("areaTexto").innerHTML = cifrado;
+        document.getElementById("copiar").style.display = "show";//Mostramos el boton de copiar.
+        document.getElementById("copiar").style.display = "inherit"; //Inherit toma el valor calculado de la propiedad de su elemento padre.
+    
+        //Codigo para utilizar el sweetalert2
+        let timerInterval
+        Swal.fire({
+        title: 'Encriptando!',
+        html: 'Cargando <b></b>',
+        timer: 500,
+        timerProgressBar: true,
+        imageUrl: "../img/catpro.gif",
+        imageWidth: 400,
+        imageHeight: 400,
+        imageAlt: 'Custom image',
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft()
+            }, 100)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+        }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer')
+        }
+        })
+    }    
 }
 
 function desencriptar(){
